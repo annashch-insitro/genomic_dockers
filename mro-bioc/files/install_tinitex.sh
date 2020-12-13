dd38a3b07d4aaa412e0da64a032f90fc49c2213c
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-12-12 21:16:52 -0500

    Minor updates to mro and mro-bioc

---
+++ mro-bioc/files/install_tinitex.sh
@@ -0,0 +1,14 @@
+Rscript -e 'install.packages("tinytex")' \
+	curl -LO -# "https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh" | sh -s - --admin --no-path \
+	&& mv ~/.TinyTeX /opt/TinyTeX \
+	&& /opt/TinyTeX/bin/*/tlmgr path add \
+	&& tlmgr install metafont mfware inconsolata tex ae parskip listings \
+	&& tlmgr path add \
+	&& Rscript -e "tinytex::r_texmf()" \
+	&& chown -R root:staff /opt/TinyTeX \
+	&& mkdir -p /opt/microsoft/ropen/$MRO_VERSION/lib64/R/site-library \
+	&& chown -R root:staff /opt/microsoft/ropen/$MRO_VERSION/lib64/R/site-library \
+	&& chmod -R g+w /opt/TinyTeX \
+	&& chmod -R g+wx /opt/TinyTeX/bin \
+	&& echo "PATH=${PATH}" >> /opt/microsoft/ropen/$MRO_VERSION/lib64/R/etc/Renviron \
+	&& Rscript -e 'install.packages("PKI")'
